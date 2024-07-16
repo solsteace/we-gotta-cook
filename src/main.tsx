88ed7f34d93f75aa46097ddff2f0966e85a27fad
Allen <valternatean13@gmail.com>
2024-07-16 09:15:13 +0700

    chore: separate global variables and style

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,8 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
 
-import { HomePage } from './pages/homePage.tsx'
+import { HomePage } from './pages/homePage/homePage.tsx'
 
 const routes = createBrowserRouter([
   {
